20674f76fcfd8253d3db47b382893b06ed126195
duc18 <duc.18052005@gmail.com>
2025-11-02 21:49:23 +0700

    Update CURD

---
+++ CSDLPTTH01/Controllers/ChiNhanhController.cs
@@ -0,0 +1,40 @@
+﻿using CSDLPTTH01.Models;
+using System.Web.Mvc;
+
+namespace CSDLPTTH01.Controllers
+{
+    public class ChiNhanhController : Controller
+    {
+        DataModel db = new DataModel();
+
+        public JsonResult Index()
+        {
+            string sql = "SELECT * FROM chinhanh";
+            return Json(db.get(sql), JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult Add(string maCN, string tenCN, string thanhpho)
+        {
+            string sql = $"INSERT INTO chinhanh(maCN, tenCN, thanhpho) VALUES('{maCN}', N'{tenCN}', '{thanhpho}')";
+            db.exec(sql);
+            return Json(new { success = true, message = "Thêm chi nhánh thành công!" });
+        }
+
+        [HttpPost]
+        public JsonResult Update(string maCN, string tenCN, string thanhpho)
+        {
+            string sql = $"UPDATE chinhanh SET tenCN=N'{tenCN}', thanhpho='{thanhpho}' WHERE maCN='{maCN}'";
+            db.exec(sql);
+            return Json(new { success = true, message = "Cập nhật chi nhánh thành công!" });
+        }
+
+        [HttpPost]
+        public JsonResult Delete(string maCN)
+        {
+            string sql = $"DELETE FROM chinhanh WHERE maCN='{maCN}'";
+            db.exec(sql);
+            return Json(new { success = true, message = "Xóa chi nhánh thành công!" });
+        }
+    }
+}
---
+++ CSDLPTTH01/Controllers/HopDongController.cs
@@ -0,0 +1,41 @@
+﻿using CSDLPTTH01.Models;
+using System.Web.Mvc;
+
+namespace CSDLPTTH01.Controllers
+{
+    public class HopDongController : Controller
+    {
+        DataModel db = new DataModel();
+
+        public JsonResult Index()
+        {
+            string sql = "SELECT * FROM hopdong";
+            return Json(db.get(sql), JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult Add(string soHD, string ngayKy, string maKH, string soDienKe, int kwDinhMuc, decimal dongiaKW)
+        {
+            string sql = $"INSERT INTO hopdong(soHD, ngayKy, maKH, soDienKe, kwDinhMuc, dongiaKW) " +
+                         $"VALUES('{soHD}', '{ngayKy}', '{maKH}', '{soDienKe}', {kwDinhMuc}, {dongiaKW})";
+            db.exec(sql);
+            return Json(new { success = true, message = "Thêm hợp đồng thành công!" });
+        }
+
+        [HttpPost]
+        public JsonResult Update(string soHD, string ngayKy, string maKH, string soDienKe, int kwDinhMuc, decimal dongiaKW)
+        {
+            string sql = $"UPDATE hopdong SET ngayKy='{ngayKy}', maKH='{maKH}', soDienKe='{soDienKe}', kwDinhMuc={kwDinhMuc}, dongiaKW={dongiaKW} WHERE soHD='{soHD}'";
+            db.exec(sql);
+            return Json(new { success = true, message = "Cập nhật hợp đồng thành công!" });
+        }
+
+        [HttpPost]
+        public JsonResult Delete(string soHD)
+        {
+            string sql = $"DELETE FROM hopdong WHERE soHD='{soHD}'";
+            db.exec(sql);
+            return Json(new { success = true, message = "Xóa hợp đồng thành công!" });
+        }
+    }
+}
